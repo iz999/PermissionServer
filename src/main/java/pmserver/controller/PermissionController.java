845033915da6aeb068d9c5891122e4f23cd8aaa6
Antony Iz Kwon <pegasus798@gmail.com>
2020-10-28 01:17:53 +0900

    주석 수정

--- src/main/java/pmserver/controller/PermissionController.java
+++ src/main/java/pmserver/controller/PermissionController.java
@@ -28,8 +28,8 @@ public class PermissionController {
 	@Autowired
 	private PermissionServerService permissionServerService;
 	
-	@RequestMapping(method = RequestMethod.POST, value = "/platformPermission")
-    public @ResponseBody PermissionServerResponse setSendData(@RequestBody HashMap<String, String> requestParam) {
+	@RequestMapping(method = RequestMethod.POST, value = "/platformPermission") //Restfull API 경로
+    public @ResponseBody PermissionServerResponse setSendData(@RequestBody HashMap<String, String> requestParam) { // 입력은 따로 VO를 만들지 않고 HashMap 으로 받는다.
 		
 		
 		HashMap<String, String> platfromPermission = new HashMap<String, String>();
@@ -38,9 +38,9 @@ public class PermissionController {
 		
 		try {
 			
-			logger.debug(new ObjectMapper().writeValueAsString(requestParam));
+			logger.debug(new ObjectMapper().writeValueAsString(requestParam)); //로그 출력으로 Object 를 json 형태로 출력
 			
-        	platfromPermission.put("permission", permissionServerService.getPlatfromPermission(requestParam));
+        	platfromPermission.put("permission", permissionServerService.getPlatfromPermission(requestParam)); // 서비스를 활용해서 결과를 권한을 가져온다.
         } 
         catch (Exception e) {
         	
@@ -50,7 +50,7 @@ public class PermissionController {
         }
         
         
-        return CommonAdapter.getPermissionServerResponse(platfromPermission, errors);
+        return CommonAdapter.getPermissionServerResponse(platfromPermission, errors); // Response를 만들어서 리턴
     }
 
 }
